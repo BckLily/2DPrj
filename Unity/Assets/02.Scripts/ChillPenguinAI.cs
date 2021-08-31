555b471f11dc880f1e80047b6d9768e4ac94c967
BckLily <52183009+BckLily@users.noreply.github.com>
2021-08-31 21:58:36 +0900

    스프라이트 수정 및 보스 컴포넌트 생성

---
+++ Unity/Assets/02.Scripts/ChillPenguinAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChillPenguinAI : MonoBehaviour
+{
+    public PolygonCollider2D polyCollider2D;
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite;
+    public Image image;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = spriteRenderer.sprite;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
